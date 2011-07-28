e595f77ab71aefd56d43fdb2a4da375e0c599383
jik <jik@93042d25-c76b-44c9-be46-c43815ad44d8>
2011-07-28 07:41:23 +0000

    Remove unneeded debugging. Work around the fact that dragexit and dragdrop don't get sent when user actually does a legitimate drop.

--- chrome/content/FolderPaneSwitcher.js
+++ chrome/content/FolderPaneSwitcher.js
@@ -7,6 +7,7 @@ Components.utils.import("resource:///modules/gloda/log4moz.js");
 //              Set cached view to null
 // onDragExit:  Cancel timer
 //              Switch to cached folder view, if any
+// onDragDrop:  Same as onDragExit
 // onDragOver:  In all folders view?
 //              Yes: Do nothing
 //              No: Has current row changed?
@@ -24,7 +25,6 @@ var FolderPaneSwitcher = {
       var prefBranch = Components.classes["@mozilla.org/preferences-service;1"]
           .getService(Components.interfaces.nsIPrefBranch);
       var show = prefBranch.getBoolPref("extensions.FolderPaneSwitcher.arrows");
-      FolderPaneSwitcher.logger.debug("show="+show);
       document.getElementById("folderPaneHeader").hidden = !show;
     }
   },
@@ -40,8 +40,19 @@ var FolderPaneSwitcher = {
     var folderTree = document.getElementById("folderTree");
     var treechildren = folderTree.getElementsByTagName("treechildren")[0];
     treechildren.addEventListener("dragenter", me.onDragEnter, false);
-    treechildren.addEventListener("dragexit", me.onDragExit, false);
     treechildren.addEventListener("dragover", me.onDragOver, false);
+    // Dragexit and dragdrop don't actually get sent when the user
+    // drops a message into a folder. This is arguably a bug in
+    // Thunderbird (see bz#674807). To work around it, I register a
+    // folder listener to detect when a move or copy is
+    // completed. This is gross, but appears to work well enough.
+    treechildren.addEventListener("dragexit", me.onDragExit, false);
+    treechildren.addEventListener("dragdrop", me.onDragExit, false);
+    var ns =
+	Components.classes["@mozilla.org/messenger/msgnotificationservice;1"]
+	.getService(Components.interfaces.nsIMsgFolderNotificationService);
+    ns.addListener(me.folderListener, ns.msgsMoveCopyCompleted|
+		   ns.folderMoveCopyCompleted);
     FolderPaneSwitcher.showHideArrowsObserver.observe();
     var prefBranch = Components.classes["@mozilla.org/preferences-service;1"]
         .getService(Components.interfaces.nsIPrefBranch);
@@ -49,33 +60,56 @@ var FolderPaneSwitcher = {
 			   FolderPaneSwitcher.showHideArrowsObserver, false);
   },
 
+  folderListener: {
+    msgsMoveCopyCompleted: function(aMove, aSrcMsgs, aDestFolder, aDestMsgs) {
+      if (aDestFolder == FolderPaneSwitcher.currentFolder) {
+	// Still remotely possible that someone else could be copying
+	// into the same folder at the same time as us, but this is
+	// the best we can do until they fix the event bug.
+	FolderPaneSwitcher.onDragExit({type:"msgsMoveCopyCompleted"});
+      }
+    },
+    folderMoveCopyCompleted: function(aMove, aSrcFolder, aDestFolder) {
+      if (aDestFolder == FolderPaneSwitcher.currentFolder) {
+	// Still remotely possible that someone else could be copying
+	// into the same folder at the same time as us, but this is
+	// the best we can do until they fix the event bug.
+	FolderPaneSwitcher.onDragExit({type:"folderMoveCopyCompleted"});
+      }
+    }
+  },
+
   onDragEnter: function() {
     FolderPaneSwitcher.logger.debug("onDragEnter");
     FolderPaneSwitcher.resetTimer();
-    FolderPaneSwitcher.currentRow = null;
     FolderPaneSwitcher.cachedView = null;
+    FolderPaneSwitcher.currentFolder = null;
   },
 
-  onDragExit: function() {
-    FolderPaneSwitcher.logger.debug("onDragExit");
+  onDragExit: function(aEvent) {
+    FolderPaneSwitcher.logger.debug("onDragExit("+aEvent.type+")");
     if (FolderPaneSwitcher.timer) {
       FolderPaneSwitcher.timer.cancel();
       FolderPaneSwitcher.timer = null;
     }
     if (FolderPaneSwitcher.cachedView) {
       gFolderTreeView.mode = FolderPaneSwitcher.cachedView;
+      FolderPaneSwitcher.cachedView = null;
     }
+    FolderPaneSwitcher.currentFolder = null;
   },
 
   onDragOver: function(aEvent) {
-    FolderPaneSwitcher.logger.debug("onDragOver");
+    FolderPaneSwitcher.logger.trace("onDragOver"); // too verbose for debug
+    var old = FolderPaneSwitcher.currentFolder;
+    FolderPaneSwitcher.currentFolder = 
+	gFolderTreeView.getFolderAtCoords(aEvent.clientX, aEvent.clientY);
     if (gFolderTreeView.mode == "all") {
       return;
     }
-    var row = gFolderTreeView.getFolderAtCoords(aEvent.clientX, aEvent.clientY);
-    if (row != FolderPaneSwitcher.currentRow) {
+    if (old != FolderPaneSwitcher.currentFolder) {
       FolderPaneSwitcher.resetTimer();
-      FolderPaneSwitcher.currentRow = row;
+      FolderPaneSwitcher.currentFolder = folder;
     }
   },
 
