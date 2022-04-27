766ca1666808114c5a6031ffae5d7a1b5de228b8
Arnd Issler <email@arndissler.net>
2022-04-27 23:00:21 +0200

    fix: prettify popup code, add missing 'let'

--- popup/update.js
+++ popup/update.js
@@ -1,18 +1,22 @@
-
 addEventListener("click", async (event) => {
-  if (event.target.id.startsWith("donate")) {
-
-    messenger.windows.openDefaultBrowser("https://www.paypal.com/donate/?hosted_button_id=KKWUR5Y3NVSHQ");
-  }
+    if (event.target.id.startsWith("donate")) {
+        messenger.windows.openDefaultBrowser(
+            "https://www.paypal.com/donate/?hosted_button_id=KKWUR5Y3NVSHQ"
+        );
+    }
 });
 
 addEventListener("load", async (event) => {
-  let text = document.body.innerHTML;
-  htmltext = text.replace(/{addon}/g, await browser.runtime.getManifest().name);
-  htmltext2 = htmltext.replace(/{version}/g, await browser.runtime.getManifest().version);
-  let browserInfo = await browser.runtime.getBrowserInfo()
-  htmltext = htmltext2.replace(/{appver}/g, browserInfo.version);
-  document.body.innerHTML = htmltext;
+    let text = document.body.innerHTML;
+    htmltext = text.replace(
+        /{addon}/g,
+        await browser.runtime.getManifest().name
+    );
+    htmltext2 = htmltext.replace(
+        /{version}/g,
+        await browser.runtime.getManifest().version
+    );
+    let browserInfo = await browser.runtime.getBrowserInfo();
+    htmltext = htmltext2.replace(/{appver}/g, browserInfo.version);
+    document.body.innerHTML = htmltext;
 });
-
-
