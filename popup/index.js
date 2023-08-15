82739e845e88ba6b247d799c508cb117a344ce64
Arnd Issler <email@arndissler.net>
2023-08-15 19:51:18 +0200

    refactor: use messenger namespace instead of browser namespace

--- popup/index.js
+++ popup/index.js
@@ -2,13 +2,13 @@ addEventListener("load", async (event) => {
     let text = document.body.innerHTML;
     htmltext = text.replace(
         /{addon}/g,
-        await browser.runtime.getManifest().name
+        await messenger.runtime.getManifest().name
     );
     htmltext2 = htmltext.replace(
         /{version}/g,
-        await browser.runtime.getManifest().version
+        await messenger.runtime.getManifest().version
     );
-    let browserInfo = await browser.runtime.getBrowserInfo();
+    let browserInfo = await messenger.runtime.getBrowserInfo();
     htmltext = htmltext2.replace(/{appver}/g, browserInfo.version);
     document.body.innerHTML = htmltext;
 });
