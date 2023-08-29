033547eccd4fffea135b1c666ce8ceb5171696f9
Arnd Issler <email@arndissler.net>
2023-08-29 23:59:09 +0200

    feat: add proper arrow icons for supernova

--- api/FPVS/implementation.js
+++ api/FPVS/implementation.js
@@ -738,7 +738,7 @@
                                             "icon-only"
                                         );
                                         buttonBackPane.style.backgroundImage =
-                                            "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAOCAYAAAAbvf3sAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QUQFgILNHreoAAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAA/ElEQVQoz42RsUrDYBCAT0dDOogEsnUUAn0VIVNx8gUCQhDpXik4uWfzCRw6Zsnq8Gf6IUggUbeQySHY0D+fW4k1sT04uOH77rg7kZEA+vXFdrt9MMa8HwPfNk1ThGFIURT1KNx1nQcU6/Ua27YREbTW9VDHKfCotWY+nyMiu9Ra1/vwPfAZBAGTyeQXvBOMMSfAZdu2b3EcY1nWH3B/wlOSJPi+Pwr2hVMRmTqOI0opOTqAG+BjuVziuu5/E9q+dA7c5XneLhaLQSFN083QWc+Al7Ismc1mw8LIh6+A1yiK8DwPEUEptTm0mwVcV1X1vVqtyLLsoNCvn4GvHw/h9W3z85x3AAAAAElFTkSuQmCC)";
+                                            "url(data:image/svg+xml;base64,PCEtLSBUaGlzIFNvdXJjZSBDb2RlIEZvcm0gaXMgc3ViamVjdCB0byB0aGUgdGVybXMgb2YgdGhlIE1vemlsbGEgUHVibGljDQogICAtIExpY2Vuc2UsIHYuIDIuMC4gSWYgYSBjb3B5IG9mIHRoZSBNUEwgd2FzIG5vdCBkaXN0cmlidXRlZCB3aXRoIHRoaXMNCiAgIC0gZmlsZSwgWW91IGNhbiBvYnRhaW4gb25lIGF0IGh0dHA6Ly9tb3ppbGxhLm9yZy9NUEwvMi4wLy4gLS0+DQo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEyIiBoZWlnaHQ9IjEyIiB2aWV3Qm94PSIwIDAgMTIgMTIiIGZpbGw9ImNvbnRleHQtZmlsbCI+DQogIDxwYXRoIGQ9Ik00Ljc1IDZsMy4yMi0zLjIyQS43NS43NSAwIDAwNi45IDEuNzJMMy4xNiA1LjQ3Yy0uMy4zLS4zLjc3IDAgMS4wNmwzLjc1IDMuNzVhLjc1Ljc1IDAgMDAxLjA2LTEuMDZ6Ii8+DQo8L3N2Zz4NCg==)";
                                         buttonBackPane.addEventListener(
                                             "click",
                                             async () => {
@@ -761,7 +761,7 @@
                                             "icon-only"
                                         );
                                         buttonNextPane.style.backgroundImage =
-                                            "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAOCAYAAAAbvf3sAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4QUQFgE6TomNWQAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAABCElEQVQoz42Sv0rDcBSFj46GdBAJZOsoBPIqQqfg5AsUhCDSPVJwcu/mEzh07NLVIZl+ECSQqFvo5FBMaPq5GKF/UnvgbPe7B869aprmfbVaPQAX+hWgTuV5vgjDkOVymQO3/0LGmIUkbNtmOp0C5Ov12uuEWqB1EAQYYwAegf5O4jYgiV6vx3A4BPgE7jegfUBry7KYzWbUdf0GXDZNc3IQaD0YDJjP5wBPpzpCcRzLcRxJ6ssYU3dtdl2XKIoAPoAbSVKSJNW+4dFoRJZlNXAHnP/FbQO+71MUBcALcLZTaxzHlSQ8z2MymQC8AledF0/TtBqPx5Rl+Q1cA9bBBoAv4Pmox5P0A+Ui9W110fJbAAAAAElFTkSuQmCC)";
+                                            "url(data:image/svg+xml;base64,PCEtLSBUaGlzIFNvdXJjZSBDb2RlIEZvcm0gaXMgc3ViamVjdCB0byB0aGUgdGVybXMgb2YgdGhlIE1vemlsbGEgUHVibGljDQogICAtIExpY2Vuc2UsIHYuIDIuMC4gSWYgYSBjb3B5IG9mIHRoZSBNUEwgd2FzIG5vdCBkaXN0cmlidXRlZCB3aXRoIHRoaXMNCiAgIC0gZmlsZSwgWW91IGNhbiBvYnRhaW4gb25lIGF0IGh0dHA6Ly9tb3ppbGxhLm9yZy9NUEwvMi4wLy4gLS0+DQo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEyIiBoZWlnaHQ9IjEyIiB2aWV3Qm94PSIwIDAgMTIgMTIiIGZpbGw9ImNvbnRleHQtZmlsbCI+DQogIDxwYXRoIGQ9Ik03LjI1IDZMNC4wMyAyLjc4QS43NS43NSAwIDAxNS4xIDEuNzJsMy43NCAzLjc1Yy4zLjMuMy43NyAwIDEuMDZsLTMuNzUgMy43NWEuNzUuNzUgMCAwMS0xLjA2LTEuMDZ6Ii8+DQo8L3N2Zz4=)";
                                         buttonNextPane.addEventListener(
                                             "click",
                                             async () => {
